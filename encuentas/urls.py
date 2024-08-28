fbc2ed0353ed486fbffe60d289d0e9c3e567e401
Pablo Igor <pablo.igor.hess@gmail.com>
2024-08-28 06:15:21 -0400

    así se entrega

--- encuentas/urls.py
+++ encuentas/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
@@ -24,5 +25,7 @@ urlpatterns = [
     path('', include('web.urls')),
     # URLs para login y logout
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+
 ]
